506970149956685102852e5ceaf9986ee20390a0
sunny-son <thstkd92@gmail.com>
2023-11-05 00:49:14 +0900

    chore : eslint rule 추가 eslint 경고에 따라 코드 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,49 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'airbnb',
+    'airbnb/hooks',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  'overrides': [
+    {
+      'env': {
+        'node': true,
+      },
+      'files': [
+        '.eslintrc.{js,cjs}',
+      ],
+      'parserOptions': {
+        'sourceType': 'script',
+      },
+    },
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
   },
-}
+  'plugins': [
+    'react',
+    'react-hooks',
+    'import',
+    'jsx-a11y',
+  ],
+  'rules': {
+    'no-console': 'warn',
+    'linebreak-style': 'off',
+    'import/extensions': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/display-name': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
