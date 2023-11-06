8736004fa1e28ec2ff23abaf121d84815bb6a74c
BeMatthewsong <songww1997@naver.com>
2023-11-06 18:12:11 +0900

    feat : 카드 레이아웃 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import GlobalStyle from '@/styles/GlobalStyle.js';
+import ListCard from '@/components/ListCard/ListCard';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
+      <ListCard />
     </div>
   );
 }
