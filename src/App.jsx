d9c4b21121dd0f60f6ccfd795d0af0bbd883c71f
BeMatthewsong <songww1997@naver.com>
2023-11-06 16:51:13 +0900

    chore : 테스트용 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import GlobalStyle from '@/styles/GlobalStyle.js';
-import Toast from '@/components/Toast';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Toast />
     </div>
   );
 }
