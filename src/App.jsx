6b7e33239b384288d8ad9b538e018fb6f09982e5
BeMatthewsong <songww1997@naver.com>
2023-11-07 10:00:31 +0900

    chore : 테스트 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import GlobalStyle from '@/styles/GlobalStyle.js';
-import ListCard from '@/components/ListCard/ListCard';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <ListCard />
     </div>
   );
 }
