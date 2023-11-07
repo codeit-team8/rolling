b2f199d7602a64ab1c34810a23f2e42a4749c78f
sunny-son <thstkd92@gmail.com>
2023-11-07 09:55:06 +0900

    style : 메시지 카드 기본 형태 스타일 추가

--- src/components/MessageCard/MessageCard.jsx
+++ src/components/MessageCard/MessageCard.jsx
@@ -1,5 +1,18 @@
-import styled from 'styled-components';
 import Badge from '@/components/MessageCard/Badge.jsx';
+import {
+  Author,
+  AuthorFrom,
+  AuthorTitle,
+  AuthorWrapper,
+  MessageBody,
+  MessageCardDivLine,
+  MessageCardProfile,
+  MessageCardTop,
+  MessageCardWrapper,
+  MessageDate,
+  ProfileImage,
+  ProfileImageWrapper,
+} from '@/components/MessageCard/MessageCard.style.jsx';
 
 function MessageCard() {
   return (
@@ -19,82 +32,10 @@ function MessageCard() {
         </MessageCardProfile>
       </MessageCardTop>
       <MessageCardDivLine />
-      <p>메시지 카드입니다.</p>
+      <MessageBody>코로나가 또다시 기승을 부리는 요즘이네요. 건강, 체력 모두 조심 또 하세요!</MessageBody>
+      <MessageDate>2023.07.08</MessageDate>
     </MessageCardWrapper>
   );
 }
 
 export default MessageCard;
-
-const MessageCardWrapper = styled.div`
-  width: 38.4rem;
-  height: 28rem;
-  border-radius: 16px;
-  background: var(--white);
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.08);
-`;
-
-const MessageCardTop = styled.div`
-  display: flex;
-  width: 100%;
-  padding: 2.8rem 2.4rem 1.5rem 2.4rem;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: flex-start;
-  gap: 1.5rem;
-`;
-
-const MessageCardProfile = styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 1.4rem;
-`;
-
-const ProfileImageWrapper = styled.div`
-  display: flex;
-  width: 5.6rem;
-  height: 5.6rem;
-  align-items: center;
-  border-radius: 100px;
-  border: 1px solid var(--gray-200);
-  background: var(--white);
-`;
-
-const ProfileImage = styled.img`
-  background: lightgray 50% / cover no-repeat;
-`;
-
-const AuthorWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 0.6rem;
-`;
-
-const AuthorTitle = styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 0.6rem;
-`;
-
-const AuthorFrom = styled.h3`
-  color: var(--black);
-  font-size: 2rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 2.4rem;
-`;
-
-const Author = styled.h3`
-  color: var(--black);
-  font-size: 2rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 2.4rem;
-`;
-
-const MessageCardDivLine = styled.div`
-  width: 33.6rem;
-  height: 1px;
-  background: var(--gray-200);
-`;
