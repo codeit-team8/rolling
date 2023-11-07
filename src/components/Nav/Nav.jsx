e91281c31fffe3fca24a24371b6e2761c831eb02
sunny-son <thstkd92@gmail.com>
2023-11-07 11:38:19 +0900

    style : Nav 바 버튼 스타일 추가

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -9,6 +9,7 @@ function Nav() {
           <Logo src={logo} alt="로고 이미지" />
           <Title>Rolling</Title>
         </NavTitle>
+        <PaperCreateButton>롤링 페이퍼 만들기</PaperCreateButton>
       </NavContainer>
     </NavWrapper>
   );
@@ -18,14 +19,13 @@ export default Nav;
 
 const NavWrapper = styled.div`
   width: 100%;
-  display: inline-flex;
   padding: 1.2rem 2rem;
-  align-items: center;
 `;
 
 const NavContainer = styled.div`
   display: flex;
   align-items: center;
+  justify-content: space-between;
   gap: 7.9rem;
 `;
 
@@ -48,4 +48,23 @@ const Title = styled.h1`
   font-style: normal;
   font-weight: 700;
   line-height: normal;
+`;
+
+const PaperCreateButton = styled.button`
+  display: flex;
+  height: 4rem;
+  padding: 0.8rem 1.6rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 6px;
+  border: 1px solid var(--gray-300, #ccc);
+  background: var(--white, #fff);
+  color: var(--gray-900, #181818);
+  text-align: center;
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.6rem;
+  letter-spacing: -0.016rem;
 `;
