50c81808c3f88b6801fc9014c7b803c9d0e61b9c
sunny-son <thstkd92@gmail.com>
2023-11-09 15:23:56 +0900

    refactor : 페이지가 아니고 컴포넌트이기 때문에 PaperList 폴더 이동

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import GlobalStyle from '@/styles/GlobalStyle.js';
 import Post from '@/pages/Post.jsx';
 import Main from '@/pages/Main.jsx';
-import RollingPaperList from '@/pages/RollingPaperList.jsx';
 
 function App() {
   return (
@@ -10,7 +9,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/list" element={<RollingPaperList />} />
+        <Route path="/list" element={<Main />} />
         <Route path="/post" element={<Post />}>
           <Route path=":postId" element={<Post />} />
         </Route>
