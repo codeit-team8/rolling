08f2c4f95b7ead638b3834e9754bd49b5e7740ed
rngpwl529 <rngpdnjs12@gmail.com>
2023-11-09 17:49:13 +0900

    chore : 단위 수정

--- src/components/Landing/LandingStyle.jsx
+++ src/components/Landing/LandingStyle.jsx
@@ -6,8 +6,8 @@ export const Section = styled.section`
   padding: 2.4rem 2.4rem 6.2278rem;
   flex-direction: column;
   align-items: center;
-  gap: 50px;
-  border-radius: 20px;
+  gap: 5rem;
+  border-radius: 2rem;
   background: var(--Surface, #f6f8ff);
   overflow: hidden;
 `;
@@ -15,10 +15,10 @@ export const Section = styled.section`
 export const Section1 = styled(Section)`
   @media (min-width: 1248px) {
     flex-direction: row;
-    padding: 60px 0px 60px 60px;
+    padding: 6rem 0 6rem 6rem;
     justify-content: flex-end;
     align-items: flex-start;
-    gap: 152px;
+    gap: 15.2rem;
   }
 `;
 
@@ -26,7 +26,7 @@ export const Section2 = styled(Section)`
   @media (min-width: 1248px) {
     flex-direction: row-reverse;
     display: flex;
-    padding: 60px 192px 60px 0px;
+    padding: 6rem 19.2rem 6rem 0;
     align-items: flex-start;
     height: 32.4rem;
     gap: 0;
@@ -36,7 +36,7 @@ export const Section2 = styled(Section)`
 export const Box = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 16px;
+  gap: 1.6rem;
   width: 100%;
 
   @media (min-width: 768px) {
@@ -57,7 +57,7 @@ export const Div = styled.div`
   ${F.FONT14B}
   width: fit-content;
   color: var(--white, #fff);
-  border-radius: 50px;
+  border-radius: 5rem;
   background: var(--purple-600, #9935ff);
   display: flex;
   padding: 0.4rem 1.2rem;
