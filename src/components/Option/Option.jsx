2cf8001c85caa1565c1f59160d14d9724582cec5
Hyeyoon <foryou9097@gmail.com>
2023-11-10 13:44:40 +0900

    feat : 클릭한 폴더별로 각각 렌더링이 되도록 하기

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -10,14 +10,29 @@ const OPTIONS = {
     'var(--blue-200, #B1E4FF)',
     'var(--green-200, #D0F5C3)',
   ],
-  backgroundImageURL: [],
+  backgroundImageURL: [
+    'https://s3-alpha-sig.figma.com/img/20c6/e481/31612bf263d24dd830adf62fd00509ed?Expires=1700438400&Signature=G7Td9B1ZIFtyu2xq1m-8cOHHytmjdvWCeqNBKHUb8ZDmZzhegq6n2eaquMjSouHxM8jV7b29ln5ouj-hXE~NvRtByTPxkdpAtmJlJtBm2TS6XMSlNMT518hrnHo0TEMQPl39sRyOVCT9Acu4ZmfzGZxeWjyN5yE71nHR6tAZMSP~d7hAR-2p6z1u5C-q9q8jrHQwliFGcD1OvfOBeIQkyimAMuFznGZfNDFR3jWoZCNc7pkGcc5jpEghbksspI2KayuqDrPo1Uys9J3sartTqkd4tiX8GREj2~Q3ACMCmH2j9UlQ4jwlDmAz8ztUGPCjhaU0cLbwIiTtDJ9eqAtSwQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    'https://s3-alpha-sig.figma.com/img/16cd/1b46/e5a1c8c110aa1788a682f388bef979a4?Expires=1700438400&Signature=B55VLD7ce7pJYy4rPzB2GPVdNZMYxG5p1OS6~iF9GAGufcVhGD9vlluyXKUYums23p3wcsBVisS1ySAWFkGcV~~7hwTUsrkdNXNIAEZq73GVyp3zmz30NLpO1rsVZQJgG~eL7-b93NZDCcHH7BESAGR6Qw4b1Tsle-l9vS0q4GCYxqmZcBaf2odUXMJ9YmLT~86zWzQyKFfcZ5e94I7AIpSx~PSfawdib5sucfYnH7HNCfNfxZA-7dNqt-Ft0FX0WY~GWHK9hykLFqo1mQ9nksDy7P63waFal4lZb5610zYJj1n8dMgRAnZNu8wz~XSGj2EVrHvsgpEOzmd8hjqj7A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+    'https://images.unsplash.com/photo-1697497305217-59590809a833?q=80&w=2036&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1699362232821-b2eccfc516e5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ],
 };
 
 function Option() {
   const [isCategorySelect, setIsCategorySelect] = useState(0);
+  const [isActive, setIsActive] = useState(OPTIONS.backgroundColor[0]);
+
+  const selectChip = {
+    color: <ColorChip />,
+    image: <ImageChip />,
+  };
 
   const handleClick = (index) => {
     setIsCategorySelect(index);
+  };
+
+  const handleColorChip = (index) => {
+    setIsActive(OPTIONS.backgroundColor[index]);
   };
 
   return (
@@ -30,16 +45,17 @@ function Option() {
         ))}
       </ButtonContainer>
       <CardContainer>
-        <ColorChip></ColorChip>
-        <ColorChip></ColorChip>
-        <ColorChip></ColorChip>
-        <ColorChip></ColorChip>
+        {isCategorySelect === 0 &&
+          OPTIONS.backgroundColor.map((color, index) => <ColorChip key={index} style={{ backgroundColor: color }} />)}
+        {isCategorySelect === 1 &&
+          OPTIONS.backgroundImageURL.map((imageURL, index) => <ImageChip key={index} backgroundImageURL={imageURL} />)}
       </CardContainer>
     </div>
   );
 }
 
 export default Option;
+
 const ButtonContainer = styled.div`
   width: 236px;
   height: 40px;
@@ -78,8 +94,26 @@ const ColorChip = styled.div`
   flex-shrink: 0;
   border-radius: 16px;
   border: 0.1rem solid rgba(0, 0, 0, 0.1);
-  background: var(--purple-200, #ecd9ff);
   box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
+
+  @media (min-width: 768px) {
+    width: 16.8rem;
+    height: 16.8rem;
+  }
+`;
+
+const ImageChip = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 15.4rem;
+  height: 15.4rem;
+  flex-shrink: 0;
+  border-radius: 16px;
+  border: 0.1rem solid rgba(0, 0, 0, 0.1);
+  box-shadow: 0 0.2rem 1.2rem 0 rgba(0, 0, 0, 0.08);
+  // background-image: url(${(props) => props.backgroundImageURL};
+  background-size: cover;
 
   @media (min-width: 768px) {
     width: 16.8rem;
