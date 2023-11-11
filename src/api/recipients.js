0eee56c9eaad33c21d9177df6d3b8d6a9d094b95
sunny-son <thstkd92@gmail.com>
2023-11-11 14:59:42 +0900

    chore : jsdoc 내용 수정

--- src/api/recipients.js
+++ src/api/recipients.js
@@ -3,9 +3,9 @@ import { DOMAIN_TEAM } from '@/api/Domain.js';
 
 /**
  * 롤링 페이퍼 대상 목록 조회 API
- * @param limit: Integer 리턴받기 원하는 질문 대상 객체 수. 기본값은 8
- * @param offset: Integer 가장 앞 객체부터 건너 뛰고 싶은 객체 수.
- * @param sort: String "Like" 를 넣으면 총 리액션 순서대로 목록 정렬
+ * @param {number} limit - 리턴받기 원하는 질문 대상 객체 수. 기본값은 8
+ * @param {number} offset - 가장 앞 객체부터 건너 뛰고 싶은 객체 수.
+ * @param {string} sort - "Like" 를 넣으면 총 리액션 순서대로 목록 정렬
  * @returns {Promise<any>}
  */
 export async function getRecipients({ limit = '', offset = '', sort = '' }) {
@@ -20,9 +20,9 @@ export async function getRecipients({ limit = '', offset = '', sort = '' }) {
 
 /**
  * 롤링 페이퍼 대상 생성 API
- * @param name : 롤링 페이퍼 대상의 이름
- * @param backgroundColor : 롤링 페이퍼 배경색. 기본값은 베이지
- * @param backgroundImageURL: 롤링 페이퍼 배경 URL.(Optional)
+ * @param {string} name - 롤링 페이퍼 대상의 이름
+ * @param {string} backgroundColor - 롤링 페이퍼 배경색. 기본값은 베이지
+ * @param {string} backgroundImageURL - 롤링 페이퍼 배경 URL.(Optional)
  * @returns {Promise<any>}
  */
 export async function postRecipients({ name, backgroundColor = 'beige', backgroundImageURL }) {
