6d6f9faf7e6385f7322e20b56625fa3d5f77906d
sunny-son <thstkd92@gmail.com>
2023-11-11 17:02:04 +0900

    feat : list path PaperList 로 연결

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Post from '@/pages/Post.jsx';
 import Main from '@/pages/Main.jsx';
 import Nav from '@/components/Nav/Nav.jsx';
 import From from '@/pages/From.jsx';
+import PaperList from '@/pages/PaperList.jsx';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/list" element={<Main />} />
+        <Route path="/list" element={<PaperList />} />
         <Route path="/post" element={<Post />}>
           <Route path=":postId" element={<Post />} />
         </Route>
