cf41b717e94a3773cc237e06c9862eb9328f4746
sunny-son <thstkd92@gmail.com>
2023-11-13 14:46:05 +0900

    refactor : 백그라운드 컬러 상수 파일 이동 및 패턴 추가

---
+++ src/util/backgroundColors.jsx
@@ -0,0 +1,11 @@
+import yellowPattern from '@/assets/images/card_pattern_yellow.png';
+import purplePattern from '@/assets/images/card_pattern_purple.png';
+import bluePattern from '@/assets/images/card_pattern_blue.png';
+import greenPattern from '@/assets/images/card_pattern_green.png';
+
+export const BACKGROUND_COLORS = [
+  { name: 'beige', color: 'var(--orange-200, #FFE2AD)', pattern: yellowPattern },
+  { name: 'purple', color: 'var(--purple-200, #ECD9FF)', pattern: purplePattern },
+  { name: 'blue', color: 'var(--blue-200, #B1E4FF)', pattern: bluePattern },
+  { name: 'green', color: 'var(--green-200, #D0F5C3)', pattern: greenPattern },
+];
